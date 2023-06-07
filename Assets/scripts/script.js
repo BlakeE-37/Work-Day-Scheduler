5593ce0ce9506009b8f876333e60bce5cdfbaac6
Blake H <blake.hollingsworth80@gmail.com>
2023-06-07 14:26:46 -0600

    added current day to the header

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -56,6 +56,9 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+  // get current day
+  var day = dayjs().format('dddd, MMMM D')
+  $('#currentDay').text(day)
   // TODO: Add code to display the current date in the header of the page.
 });
